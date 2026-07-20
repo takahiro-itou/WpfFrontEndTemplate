b3d21692f52f15782f9d0b7290549a8be9b9c9ff
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-07-20 22:30:12 +0900

    テンプレートリポジトリからプロジェクトをコピー

---
+++ ViewVb/MainWindow.xaml.vb
@@ -0,0 +1,14 @@
+﻿
+Public Class MainWindow
+
+Private m_model As MySampleModel
+
+Public Sub New()
+''--------------------------------------------------------------------
+''    コンストラクタ
+''--------------------------------------------------------------------
+    InitializeComponent()
+End Sub
+
+
+End Class
