f102cc56bbbfabb9a439955ccca8f00ec665c087
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-07-20 23:15:25 +0900

    メニューのイベントハンドラを実装

--- ViewVb/MainWindow.xaml.vb
+++ ViewVb/MainWindow.xaml.vb
@@ -9,4 +9,39 @@ Public Sub New()
 End Sub
 
 
+Private Sub runCommand(ByVal command As String)
+''--------------------------------------------------------------------
+''    指定したコマンドを実行する。
+''--------------------------------------------------------------------
+    txt
+End Sub
+
+
+Private Sub btnRun_Click(sender As Object, e As EventArgs) Handles _
+            btnRun.Click
+''--------------------------------------------------------------------
+''    「実行」ボタンのクリックイベントハンドラ。
+''
+''    入力したコマンドを実行する。
+''--------------------------------------------------------------------
+    runCommand(txtCommand.Text)
+End Sub
+
+
+Private Sub mnuFileExit_Click(ByVal sender As Object, ByVal e As EventArgs)
+''--------------------------------------------------------------------
+''    メニュー「ファイル」－「終了」
+''--------------------------------------------------------------------
+    System.Windows.Application.Current.Shutdown()
+End Sub
+
+
+Private Sub mnuRunCommand_Click(ByVal sender As Object, ByVal e As EventArgs)
+''--------------------------------------------------------------------
+''    メニュー「実行」－「コマンドを実行」
+''--------------------------------------------------------------------
+    runCommand(txtCommand.Text)
+End Sub
+
+
 End Class
