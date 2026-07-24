9f9070a4aa80eef675f6350caf5ac15cdaf1d77b
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-07-24 22:46:50 +0900

    ビューモデルクラスにイベントハンドラを実装

--- ViewVb/ViewModels/SampleViewModel.vb
+++ ViewVb/ViewModels/SampleViewModel.vb
@@ -1,7 +1,29 @@
 ﻿
+Imports System.Runtime.CompilerServices
+
+
 Namespace Global.ViewVb.ViewModels
 
 Public Class SampleViewModel
+        Implements INotifyPropertyChanged
+
+Public Sub New()
+''--------------------------------------------------------------------
+''    コンストラクタ
+''--------------------------------------------------------------------
+
+End Sub
+
+
+Public Event PropertyChanged As PropertyChangedEventHandler _
+        Implements INotifyPropertyChanged.PropertyChanged
+
+Protected Sub raisePropertyChanged(
+        <CallerMemberName> Optional propertyName As String = Nothing)
+    RaiseEvent  PropertyChanged(
+            Me, New PropertyChangedEventArgs(propertyName)
+    )
+End Sub
 
 
 End Class
