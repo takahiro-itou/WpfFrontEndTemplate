989325e8aca6a05d7341a77d23afe7d6ba061662
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-07-24 12:29:24 +0900

    作業途中

--- ViewVb/Commands/SimpleCommand.vb
+++ ViewVb/Commands/SimpleCommand.vb
@@ -1,7 +1,14 @@
 ﻿
+Imports System.Windows.Input
+
+
 Namespace Global.ViewVb.Commands
 
 Public Class SimpleCommand
+        Implements ICommand
+
+Private ReadOnly   m_execute As Action(Of Object)
+Private ReadOnly   m_canExecute As Predicate(Of Object)
 
 
 End Class
