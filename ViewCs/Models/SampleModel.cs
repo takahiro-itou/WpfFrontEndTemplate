e5dcded1488975601099fc3b528f63aefb4f1aba
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-07-25 11:01:02 +0900

    モデルクラスを仮実装

--- ViewCs/Models/SampleModel.cs
+++ ViewCs/Models/SampleModel.cs
@@ -5,14 +5,52 @@ public  class  SampleModel
 {
 
     //----------------------------------------------------------------
+    /**   モデルのタスクを実行する。
+    **
+    **/
+    public  virtual  int
+    runTask(IProgress<int>  progress)
+    {
+        executeCommand();
+        return ( 0 );
+    }
+
+
+//========================================================================
+//
+//    Properties.
+//
+
+    public  string
+    ResultText  {
+        get { return  this.m_resultText; }
+        set { this.m_resultText = value; }
+    }
+
+
+//========================================================================
+//
+//    Protected Member Functions.
+//
+
+    //----------------------------------------------------------------
     /**   指定したコマンドを実行する。
     **
     **/
-    public  virtual  void
+    protected  virtual  void
     executeCommand()
     {
+        this.ResultText = "Hello, World";
+        System.Threading.Thread.Sleep(5000);
     }
 
+
+//========================================================================
+//
+//    Member Variables.
+//
+
+    private  string     m_resultText;
 
 }   //  End class  SampleModel
 
--- ViewVb/Models/SampleModel.vb
+++ ViewVb/Models/SampleModel.vb
@@ -4,11 +4,34 @@ Namespace Global.ViewVb.Models
 Public Class SampleModel
 
 
-Public Sub executeCommand()
+Private m_resultText As String
+
+Public Property ResultText() As String
+   Get
+       Return  Me.m_resultText
+   End Get
+   Set(ByVal value As Strign)
+       Me.m_resultText = value
+   End Set
+End Property
+
+
+Public Overridable Function runTask(
+        ByVal progress As IProgress(Of Integer) ) As Integer
+''--------------------------------------------------------------------
+''    モデルのタスクを実行する。
+''--------------------------------------------------------------------
+    executeCommand()
+    runTask = 0
+End Function
+
+
+Protected Overridable Sub executeCommand()
 ''--------------------------------------------------------------------
 ''    指定したコマンドを実行する
 ''--------------------------------------------------------------------
-
+    Me.ResultText = "Hello, World"
+    System.Threading.Thread.Sleep(5000);
 End Sub
 
 
