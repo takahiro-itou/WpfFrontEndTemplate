065cb161944277fb4f81ee4adb904cec17ee0806
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-07-25 10:24:36 +0900

    モデルクラスにメンバ関数の宣言を追加

--- ViewCs/Models/SampleModel.cs
+++ ViewCs/Models/SampleModel.cs
@@ -4,6 +4,16 @@ namespace  ViewCs.Models  {
 public  class  SampleModel
 {
 
+    //----------------------------------------------------------------
+    /**   指定したコマンドを実行する。
+    **
+    **/
+    public  virtual  void
+    executeCommand()
+    {
+    }
+
+
 }   //  End class  SampleModel
 
 }   //  End of namespace  ViewCs.Models
--- ViewVb/Models/SampleModel.vb
+++ ViewVb/Models/SampleModel.vb
@@ -4,6 +4,14 @@ Namespace Global.ViewVb.Models
 Public Class SampleModel
 
 
+Public Sub executeCommand()
+''--------------------------------------------------------------------
+''    指定したコマンドを実行する
+''--------------------------------------------------------------------
+
+End Sub
+
+
 End Class
 
 End Namespace
